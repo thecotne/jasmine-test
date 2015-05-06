341c130a77b060e9a11e2308addd922d2f173bc0
cotne nazarashvili <cotne.95@gmail.com>
2015-05-06 11:47:18 +0400

    change to fat arrow syntax

--- spec/email.spec.js
+++ spec/email.spec.js
@@ -1,35 +1,35 @@
 import * as email from '../src/email.entry.js';
 
 describe('email validation', () => {
-	it('should have validation function', function() {
+	it('should have validation function', () => {
 		expect(email.validation).toBeDefined();
 	});
 
-	it('should return false if empty string is passed', function() {
+	it('should return false if empty string is passed', () => {
 		expect(email.validation('')).toBe(false);
 	});
 
-	it('should return true if valid email is passed', function() {
+	it('should return true if valid email is passed', () => {
 		expect(email.validation('cotne@itdc.ge')).toBe(true);
 	});
 
-	it('should not have spaces', function() {
+	it('should not have spaces', () => {
 		expect(email.validation('co tne@itdc.ge')).toBe(false);
 	});
 
-	it('should have dot after at symbole', function() {
+	it('should have dot after at symbole', () => {
 		expect(email.validation('cotne@itdcge')).toBe(false);
 	});
 
-	it('should contain at symbole', function() {
+	it('should contain at symbole', () => {
 		expect(email.validation('cotne.itdc.ge')).toBe(false);
 	});
 
-	it('should contain only one at symbole', function() {
+	it('should contain only one at symbole', () => {
 		expect(email.validation('cotne@it.d@c.ge')).toBe(false);
 	});
 
-	it('should not start with at symbole', function() {
+	it('should not start with at symbole', () => {
 		expect(email.validation('@gmail.com')).toBe(false);
 	});
 
